bae03763d8e9d632228536728c15b879339aa959
Tushar srivastava <tusharsrivastava1725@gmail.com>
2026-04-11 05:59:56 +0530

    Time: 4 ms (9.9%), Space: 10.6 MB (7.56%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-      string ans;
-      int count=0;
-      for(char c : s){
-        if(c=='('){
-            if(count>0) ans+=c;
-            count++;
+        std::vector<string> v;
+        int count=0;
+        int start=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') count++;
+            else count--;
+            if(count==0){
+                v.push_back(s.substr(start,i-start+1));
+                start=i+1;
+            }
         }
-        else{
-            count--;
-            if(count>0) ans+=c;
+        string ans="";
+        //here we remove the outer parenths
+        for(auto p : v){
+            ans+=p.substr(1,p.size()-2);
         }
-      }
-      return ans;  
+        return ans;
+    //   string ans;
+    //   int count=0;
+    //   for(char c : s){
+    //     if(c=='('){
+    //         if(count>0) ans+=c;
+    //         count++;
+    //     }
+    //     else{
+    //         count--;
+    //         if(count>0) ans+=c;
+    //     }
+    //   }
+    //   return ans;  
     }
 };
