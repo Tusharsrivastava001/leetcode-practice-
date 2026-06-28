659bfeb39676fd29226311ba6733efda0f39d8d1
Tushar srivastava <tusharsrivastava1725@gmail.com>
2026-06-28 14:42:30 +0530

    Time: 0 ms (100%), Space: 25 MB (5.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-     int j=0;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]!=0){
-            swap(nums[i],nums[j]);
-            j++;
+        int count=0;
+        std::vector<int> v;
+        for(int x : nums){
+            if(x==0) count++;
+            else v.push_back(x);
         }
-    }
+        while(count>0){
+            v.push_back(0);
+            count--;
+        }
+        int i=0;
+        for(int x: v){
+            nums[i++]=x;
+        }
+    //  int j=0;
+    // for(int i=0;i<nums.size();i++){
+    //     if(nums[i]!=0){
+    //         swap(nums[i],nums[j]);
+    //         j++;
+    //     }
+    // }
     }
 };
