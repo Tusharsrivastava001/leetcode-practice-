fc02acc85a76716130631fe41141622600bfd701
Tushar srivastava <tusharsrivastava1725@gmail.com>
2026-07-13 14:22:09 +0530

    Time: 0 ms (100%), Space: 10.2 MB (6.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-     std::stack<char> st;
-     for(char ch: s){
-        if(ch=='(' || ch=='[' || ch=='{'){
-            st.push(ch);
-        }
-        //yaha par sare left wale bracket me chale gaye hai
-        else{
-            if(st.empty()) return false;
-            char top=st.top();
-            if(ch==')' && top=='(' || ch==']' && top=='[' || ch=='}' && top=='{'){
+        std::stack<int> st;
+
+        for(char ch : s){
+            if(ch=='(' || ch=='{' || ch=='[') st.push(ch);
+            else{
+                if(st.empty()) return false;
+                char top=st.top();
+
+                if(ch==')' && top=='(' || ch==']' && top=='[' || ch=='}' && top=='{'){
                 st.pop();
+                }
+                else return false;
             }
-            else return false;
         }
-     }   
-     return st.empty();
+        return st.empty();
     }
 };
