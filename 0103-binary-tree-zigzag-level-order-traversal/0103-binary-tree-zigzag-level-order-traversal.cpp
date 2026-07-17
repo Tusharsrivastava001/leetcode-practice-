e523407313984a6e1c723497cb00b126635d2562
Tushar srivastava <tusharsrivastava1725@gmail.com>
2026-07-17 16:33:40 +0530

    Time: 2 ms (12.68%), Space: 15 MB (94.89%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,42 +11,37 @@
  */
 class Solution {
 public:
+    int level(TreeNode*root){
+        if(root==NULL) return 0;
+        return 1+std::max(level(root->left),level(root->right));
+    }
+    void nthlevel(TreeNode*root,int curr,int tar,std::vector<int> &v){
+        if(root==NULL) return;
+        if(curr==tar){
+            v.push_back(root->val);
+            return;
+        }
+        if(tar%2!=0){
+        nthlevel(root->left,curr+1,tar,v);
+        nthlevel(root->right,curr+1,tar,v);
+        }
+        if(tar%2==0){
+           nthlevel(root->right,curr+1,tar,v);
+            nthlevel(root->left,curr+1,tar,v); 
+        }
+    }
+     void order(TreeNode*root,std::vector<std::vector<int>> &ans){
+        int n=level(root);
+        for(int i=1;i<=n;i++){
+            std::vector<int> v;
+            nthlevel(root,1,i,v);
+            ans.push_back(v);
+            
+        }
+    }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-
-        if(root==NULL){
-            return {};
-        }
-        
-        int rev = 0;
-        queue<TreeNode *> q;
-        vector<vector<int>> ans;
-
-        q.push(root);
-
-        while(!q.empty()){
-            int n = q.size();
-            vector<int> temp;
-
-            for(int i = 0 ; i < n ; i ++){
-                TreeNode * node = q.front();
-                q.pop();
-                temp.push_back(node-> val);
-
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-            }
-
-            if(rev){
-                reverse(temp.begin(),temp.end());
-            }
-            ans.push_back(temp);
-            rev = rev ^ 1;
-        }
-
+        std::vector<vector<int>> ans;
+        order(root,ans);
         return ans;
     }
 };
